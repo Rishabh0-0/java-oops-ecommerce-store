150b79ee30e19ce90796f7171dfd450b03b69c7c
Rishabh Raj <rajrishabh2002711@gamil.com>
2025-10-30 22:13:19 +0530

    add user model

---
+++ src/main/java/model/user/Customer.java
@@ -0,0 +1,27 @@
+package model.user;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer extends User{
+    private final ShoppingCart cart;
+    private final List<Order> orderHistory;
+
+    public Customer(String userId, String username, String email, String password) {
+        super(userId, username, email, password);
+        this.cart = new ShoppingCart();
+        this.orderHistory = new ArrayList<>();
+    }
+
+    public ShoppingCart getCart() {
+        return cart;
+    }
+
+    public List<Order> getOrderHistory() {
+        return orderHistory;
+    }
+
+    public void addOrderToHistory(Order order) {
+        this.orderHistory.add(order);
+    }
+}
---
+++ src/main/java/model/user/User.java
@@ -0,0 +1,36 @@
+package model.user;
+
+public class User {
+    private final String userId;
+    private final String username;
+    private String email;
+    private String password;
+
+    public User(String userId, String username, String email, String password) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
